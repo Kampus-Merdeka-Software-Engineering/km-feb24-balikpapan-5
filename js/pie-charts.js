ece931642ac971b92e625d80aa31a26bb6cf1862
nadialvy <5026231007@student.its.ac.id>
2024-05-19 17:56:35 +0700

    adding fetch

--- js/pie-charts.js
+++ js/pie-charts.js
@@ -1,45 +1,51 @@
 function drawChart() {
-  var data = google.visualization.arrayToDataTable([
-    ["Age Group", "Percentage"],
-    ["Adults (35-64)", 49.8],
-    ["Young Adults (25-34)", 35.9],
-    ["Youth (<25)", 13.9],
-    ["Seniors (64+)", 0.4], // Adding a value for seniors to match the legend in the provided image
-  ]);
+  fetch("../data/ageGroupDistributionData.json")
+    .then((response) => response.json())
+    .then((res) => {
+      const response = res.data;
 
-  var options = {
-    height: 280,
-    // width: 200,
-    chartArea: {
-      left: 20,
-      top: 20,
-      width: "90%",
-      height: "80%",
-    },
-    colors: ["#006C80", "#0093A7", "#00C0CC", "#00E5FF"], // Updated colors to match the image
-    legend: {
-      position: "bottom",
-      alignment: "center",
+      let data = new google.visualization.DataTable();
+      data.addColumn("string", "Age Group");
+      data.addColumn("number", "Percentage");
 
-      textStyle: {
-        fontSize: 12,
-      },
-    },
-    pieSliceText: "percentage",
-    pieSliceTextStyle: {
-      color: "black",
-    },
-    tooltip: {
-      text: "percentage",
-    },
-    backgroundColor: "none",
-  };
+      for (let i = 0; i < response.length; i++) {
+        let label = response[i].label;
+        let value = response[i].value;
+        data.addRow([label, value]);
+      }
 
-  var chart = new google.visualization.PieChart(
-    document.getElementById("pie-chart")
-  );
+      let options = {
+        height: 280,
+        chartArea: {
+          left: 20,
+          top: 20,
+          width: "90%",
+          height: "80%",
+        },
+        colors: ["#006C80", "#0093A7", "#00C0CC", "#00E5FF"],
+        legend: {
+          position: "bottom",
+          alignment: "center",
 
-  chart.draw(data, options);
+          textStyle: {
+            fontSize: 12,
+          },
+        },
+        pieSliceText: "percentage",
+        pieSliceTextStyle: {
+          color: "black",
+        },
+        tooltip: {
+          text: "percentage",
+        },
+        backgroundColor: "none",
+      };
+
+      let chart = new google.visualization.PieChart(
+        document.getElementById("pie-chart")
+      );
+      chart.draw(data, options);
+    });
 }
 
 google.charts.load("current", { packages: ["corechart"] });
