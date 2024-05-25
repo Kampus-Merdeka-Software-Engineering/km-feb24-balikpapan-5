e073f8fea0de47a1727246e5aff3a20b39cb9e51
diahalfira06@gamil.com <diahalfira06@gmail.com>
2024-05-26 01:11:16 +0700

    add file js/bar-line-y.js

--- js/bar-line-y.js
+++ js/bar-line-y.js
@@ -1,93 +1,80 @@
+const distributionChart = document.getElementById("distributionChart");
+let distributionChartCanvas = null;
 
+fetch('./data/GenderDisributionByCountryandRevenue.json')
+  .then((response) => response.json())
+  .then((responseData) => {
+    const datasets = responseData.datasets[0];
+    renderGenderDistributionByCountryAndRevenue(datasets.labels, datasets.data, datasets.value);
+  });
 
-
-const disributionChart = document.getElementById("disributionChart")
-let disributionChartCanvas = null;
-
-fetch('./json/GenderDisributionByCountryRevenue.json')
-.then((response) => response.json())
-.then((response) => {
-let datasets = response.datasets[0]
-renderGenderDisributionByCountryandRevenue(datasets.labels, datasets.data, datasets.value)
-})
-
-const renderGenderDisributionByCountryandRevenue = (labels, data, value) => {
-disributionChartCanvas = new Chart(disributionChart, {
-  type: "bar",
-  data: {
-    labels: labels,
-    datasets: [
-      {
-        label: data,
-        data: value,
-        // borderWidth: 2,
-        backgroundColor: "#00838F",
-        // barThickness: 16,
-        // categoryPercentage: 9,
-        // barPercentage: 3,
+const renderGenderDistributionByCountryAndRevenue = (labels, data, value) => {
+  distributionChartCanvas = new Chart(distributionChart, {
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "Male",
+          data: data,
+          backgroundColor: "#00838F",
+        },
+        {
+          label: "Female",
+          data: value,
+          backgroundColor: "#4DD0E1",
+        }
+      ]
+    },
+    options: {
+      scales: {
+        x: {
+          beginAtZero: true,
+          ticks: {
+            callback: function (value) {
+              return value + " jt";
+            },
+          },
+        },
+        y: {
+          ticks: {
+            font: {
+              size: 10,
+            },
+          },
+        },
       },
-      {
-        label: data,
-        data: value,
-        // borderWidth: ,
-        backgroundColor: "#4DD0E1",
-        // barThickness: 16,
-        // categoryPercentage: 9,
-        // barPercentage: 3,
-      }
-    ]
-  },
-  options: {
-    indexAxis: "y",
-    scales: {
-      x: {
-        beginAtZero: true,
-        ticks: {
-          callback: function (value) {
+      plugins: {
+        legend: {
+          position: "top",
+        },
+        tooltip: {
+          callbacks: {
+            label: function (context) {
+              let label = context.dataset.label || "";
+              if (label) {
+                label += ": ";
+              }
+              if (context.parsed.x !== null) {
+                label += context.parsed.x + " jt";
+              }
+              return label;
+            },
+          },
+        },
+        datalabels: {
+          anchor: "end",
+          align: "right",
+          formatter: function (value, context) {
             return value + " jt";
           },
-        },
-      },
-      y: {
-        ticks: {
+          color: "white",
           font: {
-            size: 10, // Adjust the font size here
+            weight: "bold",
+            size: 3,
           },
         },
       },
     },
-    
-    plugins: {
-      legend: {
-        position: "top",
-      },
-      tooltip: {
-        callbacks: {
-          label: function (context) {
-            let label = context.dataset.label || "";
-            if (label) {
-              label += ": ";
-            }
-            if (context.parsed.x !== null) {
-              label += context.parsed.x + " jt";
-            }
-            return label;
-          },
-        },
-      },
-      datalabels: {
-        anchor: "end",
-        align: "right",
-        formatter: function (value, context) {
-          return value + " jt";
-        },
-        color: "white",
-        font: {
-          weight: "bold",
-          size: 3,
-        },
-      },
-    },
-  },
-  })
-}
+  });
+};
