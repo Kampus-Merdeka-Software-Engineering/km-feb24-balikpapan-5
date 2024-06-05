8f9336cabc3bdbee9ca120ad7d89eb9b73c6e692
RirinIndriyanii <ririnindriyani0401@gmail.com>
2024-06-05 16:55:10 +0700

    delete togglecheckboxes SubProduct & Product

--- js/filtering.js
+++ js/filtering.js
@@ -1,31 +1,23 @@
-// handle the dropdown
+// Menghandle Dropdown
 function toggleCheckboxes(selectBoxId, checkboxesId) {
-    const selectBox = document.getElementById(selectBoxId);
-    let expanded = false;
-  
-    selectBox.addEventListener("click", () => {
-      const checkboxes = document.getElementById(checkboxesId);
-      if (!checkboxes) {
-        console.error(`Element with ID ${checkboxesId} not found`);
-        return;
-      }
+  const selectBox = document.getElementById(selectBoxId);
+  let expanded = false;
 
-  
-      if (!expanded) {
-        checkboxes.style.display = "block";
-        expanded = true;
-      } else {
-        checkboxes.style.display = "none";
-        expanded = false;
-      }
-    });
-  }
-  
-  toggleCheckboxes("selectBoxYear", "checkboxesYear");
-  toggleCheckboxes("selectBoxCountry", "checkboxesCountry");
-  toggleCheckboxes("selectBoxAgeGroup", "checkboxesAgeGroup");
-  toggleCheckboxes("selectBoxProductCategory", "checkboxesProductCategory");
-  // toggleCheckboxes("selectBoxSubCategory", "checkboxesSubCategory");
-  toggleCheckboxes("selectBoxProduct", "checkboxesProduct");
-  
+  selectBox.addEventListener("click", () => {
+    const checkboxes = document.getElementById(checkboxesId);
+    if (!expanded) { //Jika expanded bernilai false
+      checkboxes.style.display = "block";
+      expanded = true; //checkbox terlihat
+    } else { //Jika expanded bernilai true (checkbox terlihat)
+      checkboxes.style.display = "none";
+      expanded = false; //checkbox tersembunyi
+    }
+  });
+}
+
+//Memanggil fungsi untuk beberapa pasang dropdown-checkbox
+toggleCheckboxes("selectBoxYear", "checkboxesYear");
+toggleCheckboxes("selectBoxCountry", "checkboxesCountry");
+toggleCheckboxes("selectBoxAgeGroup", "checkboxesAgeGroup");
+toggleCheckboxes("selectBoxProductCategory", "checkboxesProductCategory");
 
